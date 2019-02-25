0a9271442a4891251987a82205c37bbac56e3d26
brade <tianzhihen080710@163.com>
2019-02-25 18:33:12 +0800

    springboot shiro

--- springboot-shiro/src/main/java/com/xm/shiro/config/ShiroRealm.java
+++ springboot-shiro/src/main/java/com/xm/shiro/config/ShiroRealm.java
@@ -17,9 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-/**
- * Created by cdyoue on 2016/10/21.
- */
 
 public class ShiroRealm extends AuthorizingRealm {
     private Logger logger =  LoggerFactory.getLogger(this.getClass());
